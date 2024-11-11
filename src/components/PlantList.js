5a6a31c91f7cdb49a29e90208b63eaa2d6c03d68
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-11-11 09:11:37 +0300

    1st_plantsy_sub

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+
+function PlantList() { 
+const [plants, setplants] = useState([])
+
+
+ useEffect(() => {
+   const baseURL = "http://localhost:6001/plants"
+
+   fetch(baseURL)
+    .then(resp => resp.json())
+    .then((plantsData) => setplants(plantsData))
+
+ }, [])
+ 
+
+
+ 
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards"> 
+      {plants.map(plant => (
+        <PlantCard 
+          key={plant.id} 
+          name={plant.name} 
+          image={plant.image} 
+          price={plant.price} 
+        />
+      ))}
+    </ul>
   );
 }
 
 export default PlantList;
+
+
+
